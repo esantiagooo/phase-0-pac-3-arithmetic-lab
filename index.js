57621dff7ce986efe552c0351064379931951eda
Eric <eric.yanez50@qmail.cuny.edu>
2022-01-20 17:13:42 -0500

    done

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+    return a + b
+}
+add(1,2);
+
+function subtract(a,b){
+    return a - b
+}
+subtract(1,2);
+
+function multiply(a,b){
+    return a * b
+}
+multiply(1,2);
+
+function divide(a,b){
+    return a / b
+}
+divide(1,2);
+
+function increment(n){
+    return (n +=1)
+}
+increment(1);
+
+function decrement(n){
+    return(n -=1)
+}
+decrement(1);
+
+function makeInt(n){
+    return parseInt(n, 10)
+}
+makeInt()
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+preserveDecimal();
